be57616c6fb0beb523484ce2460618b98af32f07
Keijiro Takahashi <keijiro@gmail.com>
2015-08-19 12:40:13 +0900

    initial commit

---
+++ Assets/EventHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHandler : MonoBehaviour
+{
+    void FireEvent(AnimationEvent ev)
+    {
+        Debug.Log("state:" + ev.animatorStateInfo.shortNameHash + ", clip:" + ev.animatorClipInfo.clip.name);
+    }
+}
